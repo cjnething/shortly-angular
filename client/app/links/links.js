7e99853d9ad99dc42b243b5032e6df8eda94f94f
Hack Reactor Students <accounts@hackreactor.com>
2015-03-06 15:57:16 -0800

    tests passing, controllers complete

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,12 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+  $scope.getLinks = function() {
+    Links.getLinks().then(function(links){
+      $scope.data.links = links;
+    })
+  };
+  $scope.allLinks = $scope.data.links;
+  $scope.getLinks();
 });
